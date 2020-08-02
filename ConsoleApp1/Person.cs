25b83ec4d07b6ecec3a20e8e865eaf8a84588ed8
manhle1997 <manhle1997@github.com>
2020-08-02 15:44:41 +0700

    'First-Commit'

---
+++ ConsoleApp1/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Person
+    {
+        public void P1()
+        {
+            Console.WriteLine("phuong thuc 1");
+        }
+        public void P2()
+        {
+            Console.WriteLine("phuong thuc 2");
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    struct Book
+    {
+        private string tenSach;
+        private string tacGia;
+        private string theLoai;
+        private int maSach;
+        public void nhapThongTin(string tensach, string tacgia, string theloai, int masach)
+        {
+            tenSach = tensach;
+            tacGia = tacgia;
+            theLoai = theloai;
+            maSach = masach;
+        }
+        public void showInf()
+        {
+            Console.WriteLine("Tieu de: {0}", tenSach);
+            Console.WriteLine("Tac gia: {0}", tacGia);
+            Console.WriteLine("The loai: {0}", theLoai);
+            Console.WriteLine("Ma sach: {0}", maSach);
+            Console.WriteLine("---------------");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+            p
+            .P1()
+            .P2()
+        }
+    }
+}
